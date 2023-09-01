14aa053aa427b55f19af34c424736c8a09323d16
IDaneva <daneva_n@abv.bg>
2023-09-01 15:28:07 +0300

    created initial files for django project

---
+++ studybud/base/views.py
@@ -0,0 +1,9 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+def home(request):
+    return render(request, "home.html")
+
+
+def room(request):
+    return HttpResponse("ROOM")
