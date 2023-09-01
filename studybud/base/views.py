eefc2ee255d53fa3d6ca9358fb85db6ac9aebd84
IDaneva <daneva_n@abv.bg>
2023-09-01 17:14:07 +0300

    created templates foundation

--- studybud/base/views.py
+++ studybud/base/views.py
@@ -1,9 +1,15 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 
+rooms = [
+    {"id": 1, "name": "lets learn python"},
+    {"id": 2, "name": "Designers unite"},
+    {"id": 3, "name": "Frontend developers"},
+]
+
 def home(request):
-    return render(request, "home.html")
+    context = {"rooms": rooms}
+    return render(request, "base/home.html", context)
 
 
-def room(request):
-    return HttpResponse("ROOM")
+def room(request, pk):
+    return render(request, "base/room.html")
