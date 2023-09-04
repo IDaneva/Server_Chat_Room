cfc77e8cd6647edb863cc6c10dcf41121bb65e3b
IDaneva <daneva_n@abv.bg>
2023-09-04 16:09:19 +0300

    added room count

--- studybud/base/views.py
+++ studybud/base/views.py
@@ -11,14 +11,15 @@ from .forms import RoomForm
 
 def home(request):
     q = request.GET.get("q") if request.GET.get("q") != None else ""
-    
+
     rooms = Room.objects.filter(
         Q(topic__name__icontains=q) |
         Q(name__icontains=q) |
         Q(description__icontains=q))
     
+    room_count = rooms.count()
     topics = Topic.objects.all()
-    context = {"rooms": rooms, "topics": topics}
+    context = {"rooms": rooms, "topics": topics, "room_count": room_count}
     return render(request, "base/home.html", context)
 
 
