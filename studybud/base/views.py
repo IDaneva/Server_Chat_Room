19c38ab600bd3c4e2bc716a9fa8710dc23b6bea3
IDaneva <daneva_n@abv.bg>
2023-09-05 11:34:53 +0300

    added a registration form

--- studybud/base/views.py
+++ studybud/base/views.py
@@ -1,6 +1,6 @@
 from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
-from django.http import HttpResponse
+from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
@@ -10,11 +10,12 @@ from .forms import RoomForm
 
 
 def loginPage(request):
+    page = "login"
     if request.user.is_authenticated:
         return redirect("home")
 
     if request.method == "POST":
-        username = request.POST.get("username")
+        username = request.POST.get("username").lower()
         password = request.POST.get("password")
 
         try:
@@ -32,12 +33,29 @@ def loginPage(request):
             return redirect("home")
 
 
-    context = {}
+    context = {"page": page}
     return render(request, "base/login_register.html", context)
 
 def logoutUser(request):
     logout(request)
     return redirect("home")
+
+def registerUser(request):
+    form = UserCreationForm()
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.username = user.username.lower()
+            user.save()
+            login(request, user)
+            return redirect("home")
+        else:
+            messages.error(request, "An error ocurred. Try again")
+
+
+    context = {"form": form}
+    return render(request, "base/login_register.html", context)
 
 def home(request):
     q = request.GET.get("q") if request.GET.get("q") != None else ""
