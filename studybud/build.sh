41f0f4bfbcf99d0053a2b5109473d4d5cd0fe40c
IDaneva <daneva_n@abv.bg>
2023-09-14 14:42:56 +0300

    made changes to the build

--- studybud/build.sh
+++ studybud/build.sh
@@ -2,7 +2,9 @@
 # exit on error
 set -o errexit
 
-pip install -r ./requirements.txt
+cd studybud
+
+pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
