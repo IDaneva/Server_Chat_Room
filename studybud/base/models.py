21f0269c269c73e828cbf2df8209567c8fcbe7bb
IDaneva <daneva_n@abv.bg>
2024-01-24 21:33:17 +0200

    added extra info

--- studybud/base/models.py
+++ studybud/base/models.py
@@ -1,20 +1,20 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
 class Topic(models.Model):
     name = models.CharField(max_length=200)
 
     def __str__(self):
         return self.name
 
+
 class Room(models.Model):
     host = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
     topic = models.ForeignKey(Topic, on_delete=models.SET_NULL, null=True)
     name = models.CharField(max_length=200)
     description = models.TextField(null=True, blank=True)
-    participants = models.ManyToManyField(User,related_name="participants", blank=True)
-    updated = models.DateTimeField(auto_now=True)
-    created = models.DateTimeField(auto_now_add=True)
+    participants = models.ManyToManyField(User, related_name="participants", blank=True)
 
     class Meta:
         ordering = ["-updated", "-created"]
